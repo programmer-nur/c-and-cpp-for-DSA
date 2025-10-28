c099ca54a83268e920942b1f2b7464c0a920e7b7
programmer-nur <pesfootball83@gmail.com>
2025-10-28 21:14:10 +0600

    sloved 3,4

---
+++ extra-mid-term-problem/KhanaPinaOne.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  if (n >= 1000) {
+    printf("Three Kacchi");
+  } else if (n >= 500) {
+    printf("One Large Pizza");
+  } else if (n >= 250) {
+    printf("Three Small Burger");
+  } else if (n >= 100) {
+    printf("TThree Fuchka");
+  } else {
+    printf("Nothing");
+  }
+
+  return 0;
+}
