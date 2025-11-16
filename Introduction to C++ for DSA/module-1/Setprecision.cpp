f49b21eeec89c04250da2b599dabb057363dfbb1
programmer-nur <pesfootball83@gmail.com>
2025-11-16 21:57:03 +0600

    added eof and setprecision

---
+++ Introduction to C++ for DSA/module-1/Setprecision.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    double x = 23.225698;
+
+    cout << fixed << setprecision(2) << x;
+}
---
+++ Introduction to C++ for DSA/module-1/input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int x;
+  cin >> x;
+
+  cout << x << " " << endl;
+
+  char c = 'c';
+  //   int ascci = c;
+
+  int y = 67;
+
+  cout << (int)c << " " << (char)y << endl;
+  return 0;
+}
