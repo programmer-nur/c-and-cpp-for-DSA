8cf9d61a02165397e638c8e8a5e0446fd5abcba3
programmer-nur <pesfootball83@gmail.com>
2025-11-16 21:17:57 +0600

    add print file

---
+++ Introduction to C++ for DSA/module-1/print.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  int x = 20;
+  char c = 'A';
+  double d = 5.6;
+  std ::cout << x << " " << std::endl << c << " " << d << std::endl;
+  std ::cout << "My favorite number" << x << std::endl;
+
+  return 0;
+}
